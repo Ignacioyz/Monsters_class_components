6382ff8e5187b0c0d357b25311995c6cca0f5095
Ignacio Yz <ignacioyz@gmail.com>
2022-10-14 19:10:44 -0400

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component, useState } from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+  constructor(){
+    super();
+   this.state = {
+    name: {firstName: "Ignacio", lastName: "Yz"},
+    style: 'front-end, React'
+  }
+}
+
+
+
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Hi, my name is {this.state.name.firstName} {this.state.name.lastName}, my style is {this.state.style}
+          </p>
+         <button onClick={() => { 
+          this.setState(() => {
+           return{
+            name:{firstName:"Henry", lastName:"Jeria", style: "Musician"},
+            style:"musician"
+           }
+          });
+            console.log(this.state)
+          }}>Change Name</button> 
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
