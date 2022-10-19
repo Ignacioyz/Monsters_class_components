963819fd3a41fbed3b384889fa9d23bc581658b5
Ignacio Yz <ignacioyz@gmail.com>
2022-10-18 20:49:15 -0400

    constructor, api fetch, data map

--- src/App.js
+++ src/App.js
@@ -7,38 +7,37 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: [
-        {
-          name: "Linda",
-          id:1
-        },
-        {
-          name: "Frank",
-          id:2
-  
-        },
-        {
-          name: "Jacky",
-          id:3,
-        }, 
-        {
-        name: 'Andrei',
-        id:4
-        }
-      ]
+      // initializing the data list as empty first. 
+      monsters: [],
     }
   }
-
-
+  // how do we get the data list, where does it render, and when do I get the list? When does it render/rerender? 
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((data) =>
+        this.setState(
+          () => {
+            return { monsters: data };
+          },
+          () => {
+            console.log(this.state)
+          }
+        )
+      );
+  }
 
 
   render() {
     return (
       <div className="App">
         {
-         this.state.monsters.map((monster) => {
-          return <h1 key={monster.id} >{monster.name}</h1>
-         }) 
+          this.state.monsters.map((monster) => {
+            return <div>
+              <h1 key={monster.id}> {monster.name} </h1>
+            
+            </div>
+          })
         }
       </div>
     );
