9edd05143703fed0e7123ffd1d17ee83f772e84f
Ignacio Yz <ignacioyz@gmail.com>
2022-10-24 15:42:32 -0400

    fixed filter, created state var in render

--- src/App.js
+++ src/App.js
@@ -8,8 +8,9 @@ class App extends Component {
     this.state = {
       // initializing the data list as empty first. 
       monsters: [],
+      searchField: ''
+
     };
-    console.log('constructor')
   };
   // how do we get the data list, where does it render, and when do I get the list? When does it render/rerender? 
   componentDidMount() {
@@ -21,9 +22,6 @@ class App extends Component {
           () => {
             return { monsters: data };
           },
-          () => {
-            console.log(this.state)
-          }
         )
       );
   }
@@ -31,23 +29,26 @@ class App extends Component {
 
 
   render() {
-    console.log('render')
+
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(this.state.searchField)
+    });
     return (
       <div className="App">
         <input className='search-box'
           type='search'
           placeholder='search monsters'
           onChange={(e) => {
-            // because .includes is case sensetive 
-            const searchString = e.target.value.toLocaleLowerCase();
-            const filteredMonsters = this.state.monsters.filter((monster) => {
-              return monster.name.toLowerCase().includes(searchString)
-            });
-            this.setState(() => {
-              return { monsters: filteredMonsters };
-            })
-          }}/>
-        {this.state.monsters.map((monster) => {
+            // because .includes is case sensitive 
+            const searchField = e.target.value.toLocaleLowerCase();
+            this.setState(
+              () => {
+                return { searchField };
+              }
+            );
+          }}
+        />
+        {filteredMonsters.map((monster) => {
           return (
             <div key={monster.id}>
               <h1> {monster.name} </h1>
